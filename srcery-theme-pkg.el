03d72a1b1bc410775d3ba6516a5cc8552373e0a8
Daniel Berg <mail@roosta.sh>
2018-04-09 22:21:14 +0200

    Format package file

--- srcery-theme-pkg.el
+++ srcery-theme-pkg.el
@@ -1,1 +1,7 @@
-(define-package "srcery-theme" "0.1" "Colorscheme that focus ease of use and clearly defined contrasting colors with a slightly earthy tone." 'nil :url "https://github.com/roosta/emacs-srcery" :keywords '("color" "theme"))
+(define-package
+  "srcery-theme"
+  "0.1"
+  "Colorscheme that focus ease of use and clearly defined contrasting colors with a slightly earthy tone."
+  'nil
+  :url "https://github.com/roosta/emacs-srcery"
+  :keywords '("color" "theme"))
