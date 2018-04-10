8ea673f0d90b165901702bbff0739363894c98c3
Daniel Berg <mail@roosta.sh>
2018-04-10 19:14:27 +0200

    Delete pkg file for now

--- srcery-theme-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package
-  "srcery-theme"
-  "0.1"
-  "Colorscheme that focus ease of use and clearly defined contrasting colors with a slightly earthy tone."
-  'nil
-  :url "https://github.com/roosta/emacs-srcery"
-  :keywords '("color" "theme"))
