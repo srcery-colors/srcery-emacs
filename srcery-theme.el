c4e6c00dd6ce4a2b4a25d187542bbf8ea45e95d8
Daniel Berg <mail@roosta.sh>
2018-04-14 13:45:58 +0200

    Remove missing function call

--- srcery-theme.el
+++ srcery-theme.el
@@ -1292,8 +1292,6 @@
   (add-to-list 'custom-theme-load-path
                (file-name-as-directory (file-name-directory load-file-name))))
 
-(srcery-create-theme)
-
 (provide-theme 'srcery)
 (provide 'srcery-theme)
 
