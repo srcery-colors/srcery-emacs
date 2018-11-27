0a2e1e31471783eb8429d45b74eee2d0f8c62a43
Daniel Berg <mail@roosta.sh>
2018-11-27 17:58:01 +0100

    Change paren match to bg magenta, fg white

--- srcery-theme.el
+++ srcery-theme.el
@@ -2369,8 +2369,8 @@
 
    ;; show-paren
    `(show-paren-match
-     ((,srcery-class (:foreground ,srcery-bright-magenta :weight bold))
-      (,srcery-256-class (:foreground ,srcery-256-bright-magenta :weight bold))))
+     ((,srcery-class (:background ,srcery-magenta :foreground ,srcery-bright-white))
+      (,srcery-256-class (:background ,srcery-256-magenta :foreground ,srcery-256-bright-white))))
 
    `(show-paren-mismatch
      ((,srcery-class (:background ,srcery-red :foreground ,srcery-bright-white))
@@ -2389,8 +2389,8 @@
       (,srcery-256-class (:background ,srcery-256-magenta :foreground nil))))
 
    `(sp-show-pair-match-face
-     ((,srcery-class (:foreground ,srcery-bright-magenta :weight bold))
-      (,srcery-256-class (:foreground ,srcery-256-bright-magenta :weight bold))))
+     ((,srcery-class (:background ,srcery-magenta :foreground ,srcery-bright-white))
+      (,srcery-256-class (:background ,srcery-256-magenta :foreground ,srcery-256-bright-white))))
 
    `(sp-wrap-overlay-closing-pair
      ((,srcery-class (:background ,srcery-magenta :foreground, srcery-bright-yellow))
